9c95c7dfbaab7cc97d04ef765760b76cc83263cb
SpicyChair <63197802+SpicyChair@users.noreply.github.com>
2020-07-23 20:49:30 +0100

    Added a "File" menu with JMenu

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -2,6 +2,7 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -20,7 +21,13 @@ public class TextEditor extends JFrame {
     JButton saveButton;
     JButton loadButton;
 
-    //String filename;
+    JMenuBar menuBar;
+
+    JMenu fileMenu;
+    JMenuItem menuSave;
+    JMenuItem menuLoad;
+    JMenuItem menuExit;
+
 
     public TextEditor() {
         setTitle("Text Editor");
@@ -44,34 +51,71 @@ public class TextEditor extends JFrame {
         controlPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
         controlPanel.applyComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
+        //where user enters name of file
         filenameField = new JTextField(20);
         filenameField.setName("FilenameField");
         controlPanel.add(filenameField);
 
+        //write to file in filenameField button
         saveButton = new JButton("Save");
         saveButton.addActionListener(actionEvent -> {
 
             try (PrintWriter printWriter = new PrintWriter(new File(filenameField.getText()))) {
                 printWriter.print(textArea.getText());
-            } catch (Exception ignored) {
-            }
+            } catch (Exception ignored) {}
         });
         saveButton.setName("SaveButton");
         controlPanel.add(saveButton);
 
+        //load the file in filenameField
         loadButton = new JButton("Load");
         loadButton.addActionListener(actionEvent -> {
             textArea.setText(null);
             try {
                 textArea.setText(new String(Files.readAllBytes(Paths.get(filenameField.getText()))));
-            } catch (Exception e) {
-                System.out.println("Cannot read file: " + e.getMessage());
-            }
+            } catch (Exception ignored) {}
         });
         loadButton.setName("LoadButton");
         controlPanel.add(loadButton);
 
+        //control panel added to JFrame
         add(controlPanel, BorderLayout.NORTH);
+
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+        fileMenu = new JMenu("File");
+        fileMenu.setName("MenuFile");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        menuSave = new JMenuItem("Save");
+        menuSave.setName("MenuSave");
+        menuSave.addActionListener(actionEvent -> {
+
+            try (PrintWriter printWriter = new PrintWriter(new File(filenameField.getText()))) {
+                printWriter.print(textArea.getText());
+            } catch (Exception ignored) {}
+        });
+        fileMenu.add(menuSave);
+
+
+        menuLoad = new JMenuItem("Load");
+        menuLoad.setName("MenuLoad");
+        menuLoad.addActionListener(actionEvent -> {
+            textArea.setText(null);
+            try {
+                textArea.setText(new String(Files.readAllBytes(Paths.get(filenameField.getText()))));
+            } catch (Exception ignored) {}
+        });
+        fileMenu.add(menuLoad);
+
+        menuExit = new JMenuItem("Exit");
+        menuExit.setName("MenuExit");
+        menuExit.addActionListener(actionEvent -> dispose());
+        fileMenu.add(menuExit);
+        
+        menuBar.add(fileMenu);
+
 
         setVisible(true);
     }
